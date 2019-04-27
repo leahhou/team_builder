6929f377114c2ae9f53cff4841a2f49834befea8
Zander Zhuang <zander.zhuang@gmail.com>
2019-04-27 16:04:50 +1000

    edited routes and added initialing and team building system

---
+++ init.py
@@ -0,0 +1,3 @@
+
+
+def bootstrap_system()
--- routes.py
+++ routes.py
@@ -1,2 +1,6 @@
 from flask import render_template, request, redirect, url_for
 from server import app, system
+
+@app.route('/')
+def homepage():
+    return render_template('index.html')
