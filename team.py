70fdc14ebf60afdbb1ccd89b13f25a01b3c249f2
Zander Zhuang <zander.zhuang@gmail.com>
2019-04-28 05:35:37 +1000

    added event to team

--- team.py
+++ team.py
@@ -3,9 +3,10 @@ from id_generator import IdGenerator
 team_id = IdGenerator()
 
 class Team:
-    def __init__(self):
+    def __init__(self, event):
         self._members = []
         self._id = team_id.next()
+        self._event_id = event
 
     def add_member(self, person):
         self._members.append(person)
