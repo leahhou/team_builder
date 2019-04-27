1a8d5dd05b918622efb2c909175b87ceff864088
Zander Zhuang <zander.zhuang@gmail.com>
2019-04-27 14:47:40 +1000

    wrote id_generator and profile

---
+++ profile.py
@@ -0,0 +1,11 @@
+from id_generator import IdGenerator
+
+profile_id = IdGenerator()
+
+class Profile():
+    def __init__(self, name, langs, pos, exp, idea):
+        self._name = name
+        self._langs = langs
+        self._pos = pos
+        self._exp = exp
+        self._idea = idea
